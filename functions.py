043ded41d9b429cb2a46575788177af73b21dcd3
Swayam Barik <swayambarik@gmail.com>
2020-09-09 01:48:00 -0500

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,36 @@
+def area(width, height):
+    result = width * height
+    return result
+
+def subtract(num1, num2):
+    result = num1 - num2
+    return result
+
+def divide(num1, num2):
+    result = num1 / num2
+    return result
+
+def add(num1, num2):
+    result = num1 + num2
+    return result
+
+result = area(5,6)
+print(result)
+
+result2 = area(6,7)
+print(result2)
+
+result3 = area(7,8)
+print(result3)
+
+subresult = subtract(2,9)
+print(subresult)
+
+subresult2 = subtract(10,4)
+print(subresult2)
+
+addresult = add(2,8)
+print(addresult)
+
+addresult2 = add(20,30)
+print(addresult2)
